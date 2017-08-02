1f3fc390fd39bed271932396dae522383f8c86af
Daniel <danbanwo@gmail.com>
2017-08-02 14:18:01 -0400

    fix Button text error

--- src/components/Conversion.js
+++ src/components/Conversion.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 
 const Conversion = ({unit, handleClick}) => {
 
-  return <button style={{padding: "20px 45px"}} onClick={() => handleClick()}>Convert to {unit}</button>;
+  return <button style={{padding: "20px 45px"}} onClick={() => handleClick()}>
+          Convert to {unit === "Celsius" ? "Fahrenheit" : "Celsius"}
+        </button>;
 };
 
 export default Conversion;
